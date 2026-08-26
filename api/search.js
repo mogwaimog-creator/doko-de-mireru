f37f948ed012de14bf0efb17ebbe938acee1663a
mogwaimog-creator <mogwaimog@gmail.com>
2026-08-26 19:21:19 +0900

    search.jsを正常な状態に復旧

--- api/search.js
+++ api/search.js
@@ -1,11 +1,10 @@
 export default async function handler(req, res) {
   try {
     const query = req.query.query;
-    const movieId = req.query.id;
 
-    if (!query && !movieId) {
+    if (!query) {
       return res.status(400).json({
-        error: "映画名または作品IDが必要です"
+        error: "映画名を入力してください"
       });
     }
 
@@ -17,45 +16,23 @@ export default async function handler(req, res) {
       });
     }
 
-    let movie;
+    const searchUrl =
+      "https://api.themoviedb.org/3/search/movie" +
+      "?api_key=" + apiKey +
+      "&language=ja-JP" +
+      "&query=" + encodeURIComponent(query) +
+      "&region=JP";
 
-    if (movieId) {
-      const movieUrl =
-        "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "?api_key=" + apiKey +
-        "&language=ja-JP";
+    const searchResponse = await fetch(searchUrl);
+    const searchData = await searchResponse.json();
 
-      const movieResponse = await fetch(movieUrl);
-      const movieData = await movieResponse.json();
-
-      if (!movieData.id) {
-        return res.status(404).json({
-          error: "映画が見つかりませんでした"
-        });
-      }
-
-      movie = movieData;
-
-    } else {
-      const searchUrl =
-        "https://api.themoviedb.org/3/search/movie" +
-        "?api_key=" + apiKey +
-        "&language=ja-JP" +
-        "&query=" + encodeURIComponent(query) +
-        "&region=JP";
-
-      const searchResponse = await fetch(searchUrl);
-      const searchData = await searchResponse.json();
-
-      if (!searchData.results || searchData.results.length === 0) {
-        return res.status(404).json({
-          error: "映画が見つかりませんでした"
-        });
-      }
-
-      movie = searchData.results[0];
+    if (!searchData.results || searchData.results.length === 0) {
+      return res.status(404).json({
+        error: "映画が見つかりませんでした"
+      });
     }
+
+    const movie = searchData.results[0];
 
     const providersUrl =
       "https://api.themoviedb.org/3/movie/" +
@@ -94,11 +71,8 @@ export default async function handler(req, res) {
         "?api_key=" + apiKey +
         "&language=ja-JP";
 
-      const collectionResponse =
-        await fetch(collectionUrl);
-
-      const collectionData =
-        await collectionResponse.json();
+      const collectionResponse = await fetch(collectionUrl);
+      const collectionData = await collectionResponse.json();
 
       if (
         collectionData.parts &&
@@ -132,6 +106,7 @@ export default async function handler(req, res) {
     }
 
     return res.status(200).json({
+
       id: movie.id,
       title: movie.title,
       original_title: movie.original_title,
@@ -152,6 +127,7 @@ export default async function handler(req, res) {
             movies: seriesMovies
           }
         : null
+
     });
 
   } catch (error) {
