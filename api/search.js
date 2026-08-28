02ef9e13a71c46f29c3bfc497db5a8b3e0374bc6
mogwaimog-creator <mogwaimog@gmail.com>
2026-08-28 14:18:07 +0900

    ABEMA6

--- api/search.js
+++ api/search.js
@@ -1,27 +1,14 @@
 module.exports = async function handler(req, res) {
-
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "*"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
-  return res.status(200).json({
+  res.status(200).json({
     ok: true,
     message: "APIは正常に動作しています"
   });
-
 };
