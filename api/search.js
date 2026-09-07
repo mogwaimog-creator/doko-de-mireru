9a31e96cf158ce273f689d2b0d233b2f0ff56a89
mogwaimog-creator <mogwaimog@gmail.com>
2026-09-08 03:18:32 +0900

    Update search.js

--- api/search.js
+++ api/search.js
@@ -287,6 +287,22 @@ if (popular === "1") {
 
 }
 
+if(
+  req.query.netflixNew === "1"
+){
+
+  const newResults =
+    await getNetflixNewWorks(
+      apiKey
+    );
+
+  return res.status(200).json({
+    results:
+      newResults
+  });
+
+}
+    
  // =====================================================
 // Netflix 配信作品
 // =====================================================
@@ -4044,6 +4060,172 @@ function createMiddleDotSearchQuery(query) {
 //
 // をまとめて取得する
 // ========================================================
+
+async function getNetflixNewWorks(
+  apiKey
+){
+
+  const movieUrl =
+    "https://api.themoviedb.org/3/discover/movie" +
+    "?api_key=" +
+    encodeURIComponent(apiKey) +
+    "&language=ja-JP" +
+    "&region=JP" +
+    "&watch_region=JP" +
+    "&with_watch_providers=8" +
+    "&with_watch_monetization_types=flatrate" +
+    "&sort_by=primary_release_date.desc" +
+    "&include_adult=false" +
+    "&include_video=false" +
+    "&page=1";
+
+
+  const tvUrl =
+    "https://api.themoviedb.org/3/discover/tv" +
+    "?api_key=" +
+    encodeURIComponent(apiKey) +
+    "&language=ja-JP" +
+    "&watch_region=JP" +
+    "&with_watch_providers=8" +
+    "&with_watch_monetization_types=flatrate" +
+    "&sort_by=first_air_date.desc" +
+    "&include_adult=false" +
+    "&page=1";
+
+
+  const [
+    movieData,
+    tvData
+  ] =
+    await Promise.all([
+      fetchJson(movieUrl),
+      fetchJson(tvUrl)
+    ]);
+
+
+  const movies =
+    Array.isArray(
+      movieData &&
+      movieData.results
+    )
+      ? movieData.results.map(
+          function(movie){
+
+            return {
+              id:
+                movie.id,
+
+              title:
+                movie.title ||
+                movie.original_title ||
+                "",
+
+              poster_path:
+                movie.poster_path ||
+                null,
+
+              release_date:
+                movie.release_date ||
+                "",
+
+              vote_average:
+                Number(
+                  movie.vote_average || 0
+                ),
+
+              vote_count:
+                Number(
+                  movie.vote_count || 0
+                ),
+
+              content_type:
+                "movie"
+            };
+
+          }
+        )
+      : [];
+
+
+  const tvShows =
+    Array.isArray(
+      tvData &&
+      tvData.results
+    )
+      ? tvData.results.map(
+          function(show){
+
+            return {
+              id:
+                show.id,
+
+              title:
+                show.name ||
+                show.original_name ||
+                "",
+
+              poster_path:
+                show.poster_path ||
+                null,
+
+              release_date:
+                show.first_air_date ||
+                "",
+
+              vote_average:
+                Number(
+                  show.vote_average || 0
+                ),
+
+              vote_count:
+                Number(
+                  show.vote_count || 0
+                ),
+
+              content_type:
+                "tv"
+            };
+
+          }
+        )
+      : [];
+
+
+  return movies
+    .concat(
+      tvShows
+    )
+    .filter(
+      function(work){
+
+        return (
+          work.id &&
+          work.poster_path &&
+          work.release_date
+        );
+
+      }
+    )
+    .sort(
+      function(a,b){
+
+        return (
+          new Date(
+            b.release_date
+          ) -
+          new Date(
+            a.release_date
+          )
+        );
+
+      }
+    )
+    .slice(
+      0,
+      10
+    );
+
+}
 
 async function getProviderWorks(
   apiKey,
